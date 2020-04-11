98125cbfc87843f4d6cbb152c480fdc18e59b55e
Kubilay Caglayan <kblycaglayan@gmail.com>
2020-04-11 18:09:56 +0300

    Method added line_order

--- bin/main.rb
+++ bin/main.rb
@@ -7,41 +7,16 @@ class ReadFile
   def initialize(path)
     @path = path
     @file_name = path.match(%r{/\w*\.rb}).to_s[1..-1]
+    @read_by_lines = File.readlines(@path)
   end
 
   def to_s
-    read_by_lines
     "\nPath: #{@path}\n" \
       "File: \"#{@file_name}\" has #{count_lines} lines."
   end
 
-  def open_file
-    @opened_file = File.open(@path, 'r')
-    @opened_file.read
-  end
-
-  def close_file
-    @opened_file.close
-  end
-
-  def read_file
-    @read_file = File.read(@path)
-  end
-
-  def read_by_lines
-    @read_by_lines = File.readlines(@path)
-    line_order
-  end
-
   def count_lines
     @read_by_lines.size
-  end
-
-  def line_order
-    @line_orders = {}
-    count_lines.times do |line|
-      @line_orders[line] = 0
-    end
   end
 end
 
@@ -55,6 +30,7 @@ class ReadLines < ReadFile
     super
     match_units
     special_words
+    line_order
   end
 
   def nth_line(nth)
@@ -114,23 +90,38 @@ class ReadLines < ReadFile
     end
   end
 
-  def scan_line(line)
-    scn = StringScanner.new(nth_line(line))
-    until scn.eos?
-      matched = false
-      @match_units.size.times do |i|
-        value = scn.scan(@match_units[@keys_match_units[i]])
-        unless value.nil?
-          @count_match_units[@keys_match_units[i]] += 1
-          matched = true
-        end
-        break if matched
-      end
+  def line_order
+    @line_orders = {}
+    count_lines.times do |line|
+      @line_orders[line] = 0
+    end
+    puts 'LINE ORDER METHOD CALLED'
+  end
+
+  def increase_line_order(line)
+    @line_orders[line] += 2
+  end
+
+  def decrease_line_order(line)
+    @line_orders[line] -= 2
+  end
+
+  def change_line_order(word, line)
+    p "WORD: #{word} : LINE #{line}"
+    p word == 'end' ? decrease_line_order(line) : increase_line_order(line)
+  end
+
+  def calculate_line_order(line)
+    @line_orders.each.reduce(0) do |sum, key_value|
+      return sum if key_value[0] == line + 1
+
+      sum += key_value[1]
     end
   end
 
-  def scan_line_show(line)
-    scn = StringScanner.new(nth_line_show(line))
+  def scan_line_show(line, index)
+    scn = StringScanner.new(line)
+    p "LINE: #{index}"
     until scn.eos?
       matched = false
       unit = 0
@@ -140,6 +131,7 @@ class ReadLines < ReadFile
           p "UNIT: key: #{@keys_match_units[i]}   :  #{value}"
           @count_match_units[@keys_match_units[i]] += 1
           increase_special_word(value) if special_word?(value)
+          change_line_order(value, index) if special_word?(value)
           matched = true
           unit += 1
         end
@@ -158,8 +150,8 @@ class ReadLines < ReadFile
   end
 
   def scan_all_lines
-    count_lines.times do |line|
-      scan_line_show(line)
+    @read_by_lines.each_with_index do |line, index|
+      scan_line_show(line, index)
     end
   end
 end
@@ -173,3 +165,4 @@ puts '---'
 puts '----'
 p student_code.scan_all_lines
 p student_code.line_orders
+p student_code.special_w_count
