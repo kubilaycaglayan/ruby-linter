0e3fb3435a8cc4939459d429fbf89924408f78eb
Kubilay Çağlayan <kblycaglayan@gmail.com>
2020-04-13 22:20:16 +0300

    attr_accessor changed to attr_reader

--- lib/check_errors.rb
+++ lib/check_errors.rb
@@ -1,7 +1,7 @@
 require_relative 'index_code.rb'
 
 class CheckErrors < IndexCode
-  attr_accessor :error_counter, :errors
+  attr_reader :error_counter, :errors
   def initialize(path)
     @error_counter = 0
     super
